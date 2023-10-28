81e901569df5e168891cda81e61d86c784489849
Aya-Abdelmoez <ayaabdelmoez172@gmail.com>
2023-10-28 23:38:55 +0200

    header-sidebar-post

---
+++ ClientSide/src/post/Post.jsx
@@ -0,0 +1,27 @@
+import "./post.css"
+
+export default function 
+() {
+  return (
+    <div className='post'>
+     <img 
+     className="postImg"
+     src="https://static.vecteezy.com/system/resources/thumbnails/025/284/015/small/close-up-growing-beautiful-forest-in-glass-ball-and-flying-butterflies-in-nature-outdoors-spring-season-concept-generative-ai-photo.jpg"
+     alt="pr"
+     />
+     <div className="postInfo">
+        <div className="postCats">
+         <span className="postCat">Music Life</span>
+        </div>
+        <span className="postTitle">
+            Lorem ipsum dolor sit amet 
+        </span>
+        <hr/>
+         <span className="postDate">1 hour ago</span>
+     </div>
+     <p className="postDesc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Consequatur, quas ullam quidem culpa eos maxime earum architecto, 
+        iste ducimus nisi itaque labore officiis incidunt. Consectetur non ipsum maiores quibusdam in.</p>
+    </div>
+  )
+}
