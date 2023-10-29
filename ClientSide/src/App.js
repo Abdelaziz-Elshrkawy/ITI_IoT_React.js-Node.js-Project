8e22ad805ef772823ac18cd183cdf16432b1ecc8
Abdelaziz-Elshrkawy <abdelaziz_elshrkawy53@yahoo.com>
2023-10-29 05:41:23 +0200

    error handling

--- ClientSide/src/App.js
+++ ClientSide/src/App.js
@@ -1,20 +1,13 @@
 import TopBar from "./Topbar/TopBar";
-<<<<<<< HEAD
 import Home from "./pages/home/Home";
-=======
 import Login from "./login/Login";
->>>>>>> 37b2d1a25926bb1003a3e956a1078ecd746523ed
 
 function App() {
   return (
     <>
       <TopBar />
-<<<<<<< HEAD
       <Home />
-
-=======
       <Login />
->>>>>>> 37b2d1a25926bb1003a3e956a1078ecd746523ed
     </>
 
   );
