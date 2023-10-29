bcbbe6396af60fc0e1b2dd85c2d5b1942b438184
Aya-Abdelmoez <ayaabdelmoez172@gmail.com>
2023-10-29 20:39:13 +0200

    all

--- ClientSide/src/App.js
+++ ClientSide/src/App.js
@@ -1,33 +1,27 @@
-<<<<<<< HEAD
-// import TopBar from "./components/Topbar/TopBar";
-// import Home from "./pages/home/Home";
-// import Login from "./login/Login";
-// import Single from "./pages/single/Single";
-// import Write from "./pages/write/Write";
-=======
-import TopBar from "./Topbar/TopBar";
+
+import TopBar from "./components/Topbar/TopBar";
+import Home from "./pages/home/Home";
+import Login from "./login/Login";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Register from './Register/Register'
+
+
 import Settings from "./pages/settings/Settings";
 
->>>>>>> 3ef2fcea7f4dedd0f78f0d9cfb0acaea665eada2
+
 
 function App() {
   return (
-    
-<<<<<<< HEAD
-  <></>
-      // <TopBar />
-      // <Home />
-      {/* <Login /> */}
-      {/* <Single /> */}
-      {/* <Write /> */}
-=======
-    <TopBar/>
-    <Settings/>
->>>>>>> 3ef2fcea7f4dedd0f78f0d9cfb0acaea665eada2
-  </>
-
-  
-
+    <>
+       <TopBar />
+       <Home />
+       <Settings/>
+       <Login />
+       <Register />
+     
+     
+    </>
   );
 }
 
--- ClientSide/src/pages/home/Home.jsx
+++ ClientSide/src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../../components/header/Header'
-import Sidebar from '../../components/sidebar/Sidebar'
+import Sidebar from '../../components/sidebar/Sidebar.jsx'
 import Posts from '../../components/posts/Posts'
 
 export default function Home (){
