3ef2fcea7f4dedd0f78f0d9cfb0acaea665eada2
ManarAboEmira <maemira50001@gmail.com>
2023-10-29 20:00:15 +0200

    Settings

--- ClientSide/src/App.js
+++ ClientSide/src/App.js
@@ -1,18 +1,14 @@
 import TopBar from "./Topbar/TopBar";
-import Home from './pages/home'
 import Settings from "./pages/settings/Settings";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
-    <Router>
-      <TopBar />
-      <switch>
-        <Route path="/">
-          <Home />
-        </Route>
-      </switch>
-    </Router>
+    <>
+    <TopBar/>
+    <Settings/>
+    </>
+
   );
 }
 
