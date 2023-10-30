2fd2072ad495771b2ccc75e9a4034ccf8ba71dd5
ManarAboEmira <maemira50001@gmail.com>
2023-10-30 13:48:55 +0200

    Routing

--- ClientSide/src/App.js
+++ ClientSide/src/App.js
@@ -1,25 +1,37 @@
-
-import TopBar from "./components/Topbar/TopBar";
-import Home from "./pages/home/Home";
-import Login from "./login/Login";
-import Single from "./pages/single/Single";
-import Write from "./pages/write/Write";
-import Register from './Register/Register'
-
-
-import Settings from "./pages/settings/Settings";
-
-
+import TopBar from "./components/Topbar/TopBar.jsx";
+import Home from "./pages/home/Home.jsx";
+import Register from "./Register/Register.jsx";
+import NotFound from "./NotFound.jsx";
+import Login from "./login/Login.jsx";
+import Single from "./pages/single/Single.jsx";
+import Write from "./pages/write/Write.jsx";
+import Settings from "./pages/settings/Settings.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
-    <>
+    <Router>
       <TopBar />
-      <Home />
-      <Settings />
-      <Login />
-      <Register />
-    </>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/posts" element={<Home />} />
+        <Route
+          exact
+          path="/register"
+          element={user ? <Home /> : <Register />}
+        />
+        <Route exact path="/login" element={user ? <Home /> : <Login />} />
+        <Route exact path="/post/:id" element={<Single />} />
+        <Route exact path="/write" element={user ? <Write /> : <Register />} />
+        <Route
+          exact
+          path="/settings"
+          element={user ? <Settings /> : <Register />}
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
--- ClientSide/src/components/Topbar/TopBar.jsx
+++ ClientSide/src/components/Topbar/TopBar.jsx
@@ -1,6 +1,8 @@
+import { Link } from 'react-router-dom';
 import './TopBar.css'
 
 export default function TopBar() {
+const user =false;
   return (
     <div className='top'>
       <div className="topleft">
@@ -11,17 +13,33 @@ export default function TopBar() {
       </div>
       <div className="topCenter">
         <ul className="topList">
-          <li className='topListItem'>HOME</li>
-          <li className='topListItem'>ABOUT</li>
-          <li className='topListItem'>CONTACT</li>
-          <li className='topListItem'>WRITE</li>
-          <li className='topListItem'>LOGOUT</li>
+          <li className='topListItem'>
+            <Link to="/" className='link'>HOME</Link>
+          </li>
+          <li className='topListItem'><Link to="/" className='link'>ABOUT</Link></li>
+          <li className='topListItem'><Link to="/" className='link'>CONTACT</Link></li>
+          <li className='topListItem'><Link to="/write" className='link'>WRITE</Link></li>
+          <li className='topListItem'>{ user && "LOGOUT"}</li>
         </ul>
       </div>
       <div className="topRight">
-        <img 
-        className="topImg"
-        src="https://www.katebackdrop.com/cdn/shop/articles/Photo_by_David_Werbrouck_on_Unsplash.jpg?v=1665711487" alt=""></img>
+        {
+          user? (<img 
+            className="topImg"
+            src="https://www.katebackdrop.com/cdn/shop/articles/Photo_by_David_Werbrouck_on_Unsplash.jpg?v=1665711487" alt=""></img>) :
+            (<ul className='topList'>
+              <li className='topListItem'>
+              <Link to="/Login" className='link'>LOGIN
+              </Link>
+              </li>
+              <li className='topListItem'>
+              <Link to="/Register" className='link'>REGISTER
+              </Link>
+              </li>
+            </ul>
+            )
+        }
+
         <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
       </div>
     </div>
