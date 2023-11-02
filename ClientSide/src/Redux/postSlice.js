ed9a10d7f95f40c4756377e5ec30188f3d58228b
Abdelaziz-Elshrkawy <abdelaziz_elshrkawy53@yahoo.com>
2023-11-02 09:18:04 +0200

    updates

--- ClientSide/src/Redux/postSlice.js
+++
@@ -1,9 +0,0 @@
-import { createSlice } from '@reduxjs/toolkit'
-
-const postSlice = createSlice({
-    name: 'post',
-    initialState: { posts: null },
-    reducer: {}
-})
-
-export default postSlice.reducer;
--- ClientSide/src/Redux/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import user from './userSlice'
-import post from './postSlice'
-export default configureStore({
-    reducer: {
-        user,
-        post
-    }
-})
