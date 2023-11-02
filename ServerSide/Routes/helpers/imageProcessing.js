536a8850a13f6d4b13b339eda9e8365ac33c5a27
Abdelaziz-Elshrkawy <abdelaziz_elshrkawy53@yahoo.com>
2023-11-02 09:23:00 +0200

    updates

--- ServerSide/Routes/helpers/imageProcessing.js
+++
@@ -1,6 +0,0 @@
-import multer, { memoryStorage } from 'multer';
-
-const imageProcessing = (fieldName) => {
-    return multer({ storage: memoryStorage() }).single(fieldName);
-};
-export default imageProcessing;
