c84b99a537cf7c57823f7cde5a7dcf72c787a200
Abdelaziz-Elshrkawy <106397839+Abdelaziz-Elshrkawy@users.noreply.github.com>
2023-11-11 06:05:48 +0000

    replit

--- ServerSide/index.js
+++ ServerSide/index.js
@@ -8,19 +8,19 @@ const app = new express();
 
 app.use(express.json());
 app.use(
-    cors({
-        origin: 'https://iot-graduation-project-client.onrender.com',
-    }),
+  cors({
+    origin: 'https://iot-graduation-project-client.onrender.com',
+  }),
 );
 app.use('/user', userRoute);
 app.use('/post', postRoute);
 app.use((err, req, res, next) => {
-    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-        res.status(400).send({ response: 'Invalid JSON format' });
-    } else {
-        next();
-    }
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    res.status(400).send({ response: 'Invalid JSON format' });
+  } else {
+    next();
+  }
 });
 app.listen(port, () => {
-    console.log(`Running on: http://localhost:${port}`);
+  console.log(`Running on: http://localhost:${port}`);
 });
